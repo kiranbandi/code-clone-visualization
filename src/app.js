35c2de85e51d4ea1545f73bc4d04de7c58986e2d
Kiran Bandi <bvenkatkiran@gmail.com>
2018-03-25 17:51:56 -0600

    update gcad output files and snippet to parse data

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 import * as d3 from 'd3';
 import axios from 'axios';
-import convert from 'xml-js';
-
-console.log("Enter Code Setup")
+import processGcadOutput from './processGcadOutput';
 
 // Loading the synteny collinearity file
-axios.get('assets/files/clone.xml').then(function (response) {
+axios.get('assets/files/gcad_wget_output.txt').then(function (response) {
 
-    var cloneData = convert.xml2js(response.data, { compact: true, spaces: 0 });
-    console.log(cloneData);
+    let processedCloneData = processGcadOutput(response);
+    console.log(processedCloneData);
 
-}).catch(function () {
-    console.log("There was an error in loading the clone XML file");
+}).catch(function (error) {
+    console.log(error)
+    console.log("There was an error in loading the clone genealogy file");
 })
-
---
+++ src/processGcadOutput.js
@@ -0,0 +1,78 @@
+
+export default function (cloneResponse) {
+    
+    let responseArray = cloneResponse.data.split("\n"),
+        sourceName = responseArray[0].split("\\").slice(-1).join(),
+        sourceCloneInfo = responseArray.slice(1, 5).join('\n');
+
+    // Genealogy of every clone set is represented in a single line that starts with "[Version" so we look for every line that starts with that 
+    // and then process that line and the next 7 lines following it as a bunch because they contain info regarding this clone set
+    let lineIndex = 0,
+        genealogyList = [];
+    while (lineIndex < responseArray.length) {
+        if (responseArray[lineIndex].indexOf('[Version') > -1) {
+            let cloneClass = responseArray[lineIndex],
+                parsedSet = splitAndParseCloneSet(responseArray[lineIndex]);
+            if (parsedSet.length > 0) {
+                genealogyList.push({
+                    'set': parsedSet,
+                    // Following 8 lines contain the information regarding that clone set genealogy so we store that and skip those lines
+                    // to parse through the list faster
+                    'info': responseArray.slice(lineIndex + 1, lineIndex + 8).join('\n')
+                })
+            }
+            lineIndex += 7
+        }
+        // increase count by 1 , also taken into count  when we need to skip 8 lines , icremented 7 times inside if block and once outside
+        lineIndex += 1;
+    }
+    return genealogyList;
+}
+
+function splitAndParseCloneSet(cloneSetString) {
+    // Split the entire string by tab and then read through the list sequentially
+    // since we will be getting the genealogy information between 2 clones at a time 
+    // we will be grouping the result into clone sets so 1->2->3->4 would be grouped as [{1,2},{2,3},{3,4}]
+
+    // If a segment has the string "version" in it it means it has the information regarding the clone 
+    // and the next two segments are the change type and the next version of the clone
+    // so we loop through the tab split list in groups of 3 segments
+    let cloneSetStringSplit = cloneSetString.split('\t'),
+        loopIndex = 0,
+        changePairList = [];
+    // ignored clone sets that have change type split or dissappeared  - TEMPORARY FIX !!
+    if (cloneSetString.indexOf('split') > -1 || cloneSetString.indexOf('disappeared') > -1) {
+        return [];
+    }
+    // Since we move through the list in sets of three we go forward only if the first segment has the string version in it 
+    // and the segment following it is not empty 
+    // to check if the string is not empty we use split and join trick to remove spaces and then check to see if it has length more than 1
+    while ((loopIndex < cloneSetStringSplit.length) & (cloneSetStringSplit[loopIndex].indexOf('Version') > -1) & (cloneSetStringSplit[loopIndex + 1].split(' ').join('').length > 1)) {
+
+        // for source and target remove the leading and trailing square brackets and split the segment using the comma inbetween
+        let source = cloneSetStringSplit[loopIndex].slice(1, -1).split(','),
+            changeType = cloneSetStringSplit[loopIndex + 1],
+            target = cloneSetStringSplit[loopIndex + 2].slice(1, -1).split(',');
+        // To get the version from the segment we isolate version by splitting with ':' and then get the second part 
+        // we then remove spaces from this part using split and join  
+        // To get  cloneType we just look for the last six characters in the segment from the last 
+        // and for classId we split using space the second part after splitting with comma 
+        // and then pick the number from the resulting list 
+
+        changePairList.push({
+            'source': {
+                'version': source[0].split(':')[1].split(' ').join(''),
+                'cloneType': source[1].slice(-6),
+                'classId': Number(source[1].split(" ").slice(-2, -1).join(''))
+            },
+            'target': {
+                'version': target[0].split(':')[1].split(' ').join(''),
+                'cloneType': target[1].slice(-6),
+                'classId': Number(target[1].split(" ").slice(-2, -1).join(''))
+            },
+            'changeType': cloneSetStringSplit[loopIndex + 1]
+        })
+        loopIndex += 2
+    }
+    return changePairList;
+}
